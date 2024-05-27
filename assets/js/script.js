70db5cfbc46960eba6e7feff741ac4542b25a75a
rray-h <114110100+rray-h@users.noreply.github.com>
2024-05-27 19:51:21 +0300

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-const burger = document.querySelector('.header__burger');
-const lines = document.querySelectorAll('.burger__line');
-
-function toggleBurger() {
-  lines.forEach((line) => line.classList.toggle('active'));
-}
-
-burger.addEventListener('click', toggleBurger);
-
